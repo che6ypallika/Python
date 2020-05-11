c7767e2d08676c5c0e1af755a20f4f8c1ee04847
Nikita <vladimirov.n@gmail.com>
2020-05-11 22:42:32 +0300

    Webinar input

---
+++ lesson 1.py
@@ -0,0 +1,42 @@
+# PEP8
+# Комментарий комметариевич
+# name = input('Ваше имя?\n')
+while True:
+    age = input('Возвраст, число \n')
+    if age.isdigit():
+        age = int(age)
+        break
+    else:
+        print('Введите чЕсло')
+
+print(age)
+# this_year = 2020
+#
+# if age.isdigit():
+#     age = int(age)
+#     b_year = this_year - age
+#     print(b_year)
+#     while True:
+#         if not age:
+#             break
+#         print(age)
+#         age -= 1
+#
+    # if age >= 18:
+    #     print('УРА!')
+    # elif age >= 16:
+    #     print('Есть эротика')
+    # elif 12 >= age > 8:
+    #     print('Есть новое аниме')
+    # else:
+    #     print('Есть смешарики!')
+
+#
+# hello_str = "Hello,bro!"
+# hello_str.isdigit()
+# var_int = 33
+# var_int2 = 44
+#
+# var_int3 = var_int + var_int2
+#
+
